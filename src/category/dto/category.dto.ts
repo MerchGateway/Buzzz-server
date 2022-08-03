ad5782afb33ab74166810c91c8bf43a661a182c2
eddy madu <eddymadu90@gmail.com>
2022-08-03 15:07:54 +0100

    chore:added a contraint for fields in the entity

--- src/category/dto/category.dto.ts
+++ src/category/dto/category.dto.ts
@@ -1,5 +1,13 @@
+import { IsString, MinLength, MaxLength } from 'class-validator';
+
 export class CreateCategoryDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(16)
   public readonly name: string;
+  @IsString()
+  @MinLength(5)
+  @MaxLength(80)
   public readonly description: string;
 }
 export class UpdateCategoryDto {
