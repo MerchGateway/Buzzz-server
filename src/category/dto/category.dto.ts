b18c56e632afe9105086f83c499a8714487fa9dd
eddy madu <eddymadu90@gmail.com>
2022-08-03 16:09:43 +0100

    fix:added the isoptional decorator to the UpdateCategoryDto

--- src/category/dto/category.dto.ts
+++ src/category/dto/category.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, MinLength, MaxLength } from 'class-validator';
+import { IsString, MinLength, MaxLength, IsOptional } from 'class-validator';
 
 export class CreateCategoryDto {
   @IsString()
@@ -14,9 +14,11 @@ export class UpdateCategoryDto {
   @IsString()
   @MinLength(3)
   @MaxLength(16)
+  @IsOptional()
   public readonly name?: string;
   @IsString()
   @MinLength(5)
   @MaxLength(80)
+  @IsOptional()
   public readonly description?: string;
 }
