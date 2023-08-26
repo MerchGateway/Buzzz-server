9d2cbe51caebd1ec8b816efd9707b307241652cd
Eddy Madu <eddymadu90@gmail.com>
2023-08-26 05:04:27 -0700

    chore:Clean up

--- src/app/analytics/entities/analytics.entity.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Analytics = void 0;
-var Analytics = /** @class */ (function () {
-    function Analytics() {
-    }
-    return Analytics;
-}());
-exports.Analytics = Analytics;
--- src/types/status.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Status = void 0;
-var Status;
-(function (Status) {
-    Status["ENABLED"] = "enabled";
-    Status["DISABLED"] = "disabled";
-})(Status = exports.Status || (exports.Status = {}));
