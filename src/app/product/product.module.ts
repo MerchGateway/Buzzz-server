924abd09e91fde73bc18d1b63c45b006cb0c5cb8
Eddy Madu <eddymadu90@gmail.com>
2023-09-09 06:24:25 -0700

    fix:missing fields

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -4,11 +4,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './product.entity';
 import { ProductService } from './product.service';
 import { CategoryModule } from '../category/category.module';
+import { CLOUDINARY } from 'src/constant';
+import { CloudinaryProvider } from 'src/providers/cloudinary.provider';
+import { ConfigService } from '@nestjs/config';
+
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product]), CategoryModule],
   controllers: [ProductController],
-  providers: [ProductService, CategoryModule],
+  providers: [ProductService, CategoryModule,  {
+    provide: CLOUDINARY,
+    useFactory: (configService: ConfigService) => {
+      return new CloudinaryProvider(configService);
+    },
+    inject: [ConfigService],
+  },],
   exports: [ProductService],
 })
 export class ProductModule {}
