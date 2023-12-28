325bb8a9dd96b996485d50c15c74e086968a0594
Eddy Madu <eddymadu90@gmail.com>
2023-12-28 20:25:34 +0000

    fix:update order status and wrong footer  in new gift mail

--- src/app/order/dto/order.dto.ts
+++ src/app/order/dto/order.dto.ts
@@ -1,4 +1,4 @@
-import { IsOptional, IsArray, IsObject } from 'class-validator';
+import { IsOptional, IsArray, IsObject, IsEnum } from 'class-validator';
 import { Status } from '../../../types/order';
 export class CreateOrderDto {
   @IsObject()
@@ -11,8 +11,10 @@ export class CreateOrderDto {
   };
 }
 export class UpdateOrderDto {
+  @IsOptional()
   @IsArray()
   cart?: any[];
+  @IsEnum(Status)
   @IsOptional()
   status?: Status;
 }
