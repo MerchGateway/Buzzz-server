aae38dd48cbe4676aa68cb72255687b78181f8a6
meg0m1nd <49687954+meg0m1nd@users.noreply.github.com>
2024-01-03 21:51:45 +0100

    Update order.service.ts

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -237,18 +237,24 @@ export class OrderService {
     }
     const { limit, page, route } = pagination;
     const qb = this.orderRepository.createQueryBuilder('order');
-    qb.leftJoin('order.product', 'product').select('product.name')
-    .leftJoin('order.user', 'user')
-      .select('user.id')
-      .addSelect('order.quantity')
-      .addSelect('order.createdAt')
-      .addSelect('order.total')
-      .addSelect('order.status');
-    qb.where('order.seller_id = :sellerId OR order.user.id =:userId', {
-      sellerId: user.id,
-      userId: user.id,
-    });
-    qb.orderBy('order.created_at', 'DESC');
+qb
+  .leftJoinAndSelect('order.product', 'product')
+  .leftJoinAndSelect('order.user', 'user')
+  .select([
+    'product.name',
+    'user.id',
+    'order.quantity',
+    'order.createdAt',
+    'order.total',
+    'order.status',
+  ])
+  .where('order.seller_id = :sellerId OR order.user.id = :userId', {
+    sellerId: user.id,
+    userId: user.id,
+  })
+  .orderBy('order.created_at', 'DESC');
+
+const result = await qb.getMany();
 
     return paginate<Order>(qb, { limit, page, route });
   }
